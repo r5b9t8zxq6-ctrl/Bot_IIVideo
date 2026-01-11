a432c5943b91e328277fa0b6b5edbc6e5c28f224
r5b9t8zxq6-ctrl <r5b9t8zxq6@privaterelay.appleid.com>
2026-01-11 21:43:34 +0300

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-from logger import setup_logger
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-)
-
-logger = logging.getLogger("ai-studio-bot")
