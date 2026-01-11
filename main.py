fd8686bd2331f35c188df996ebec8c15e7e034aa
r5b9t8zxq6-ctrl <r5b9t8zxq6@privaterelay.appleid.com>
2026-01-11 20:27:24 +0300

    Initialize FastAPI app with webhook support

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Request
+
+from bot import bot, dp
+from router import router
+from worker import worker
+from config import FULL_WEBHOOK_URL, WEBHOOK_PATH
+from logging import logger
+
+dp.include_router(router)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Startup")
+    await bot.set_webhook(FULL_WEBHOOK_URL)
+    task = asyncio.create_task(worker())
+    yield
+    task.cancel()
+    await bot.delete_webhook()
+    logger.info("Shutdown")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post(WEBHOOK_PATH)
+async def webhook(request: Request):
+    update = await request.json()
+    await dp.feed_raw_update(bot, update)
+    return {"ok": True}
